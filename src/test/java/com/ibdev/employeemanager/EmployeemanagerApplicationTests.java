1ff8957dc33a5ede4977854fbaca021377e9efb7
UPRHNK <vpvxnc@gmail.com>
2022-11-10 21:57:32 +0300

    Commit #1

---
+++ src/test/java/com/ibdev/employeemanager/EmployeemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibdev.employeemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeemanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
