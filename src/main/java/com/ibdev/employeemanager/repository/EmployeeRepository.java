af9b02a0b54f0520d7375a639f42802e40077d88
UPRHNK <vpvxnc@gmail.com>
2022-11-10 22:19:14 +0300

    Commit #3

--- src/main/java/com/ibdev/employeemanager/repository/EmployeeRepository.java
+++ src/main/java/com/ibdev/employeemanager/repository/EmployeeRepository.java
@@ -3,7 +3,11 @@ package com.ibdev.employeemanager.repository;
 import com.ibdev.employeemanager.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
+    void deleteEmployeeById(Long id);
 
+    Optional findEmployeeById(Long id);
 }
